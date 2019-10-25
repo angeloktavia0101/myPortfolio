496c1117adc0c8dd079750b2941794f94dc256bf
Yusrizall <56021570+Yusrizall@users.noreply.github.com>
2019-10-25 10:10:41 +0700

    menambah script di script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const switchButton = document.getElementById('customSwitches');
 const teksSwitch = document.getElementById('teks');
+const tablerow = document.getElementsByTagName('tr');
 
 switchButton.addEventListener('click',() => {
 
 	document.body.classList.toggle('darkBody');
+
+	for(let i = 0; i < tablerow.length; i++){
+		tablerow[i].classList.toggle('whiteColor');
+	}
 
 	if(document.body.classList.contains('darkBody')){
 		teksSwitch.innerHTML = 'Dark';
